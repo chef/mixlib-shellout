d5751edf1a9238b20a20d63d54b507591f7831f9
Tim Smith <tsmith@chef.io>
2018-12-02 21:18:14 -0800

    Wire up Expeditor
    Signed-off-by: Tim Smith <tsmith@chef.io>

---
+++ .expeditor/update_version.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# After a PR merge, Chef Expeditor will bump the PATCH version in the VERSION file.
+# It then executes this file to update any other files/components with that new version.
+#
+
+set -evx
+
+VERSION=$(cat VERSION)
+
+sed -i -r "s/^(\\s*)VERSION = \".+\"/\\1VERSION = \"$VERSION\"/" lib/mixlib/shellout/version.rb
+
+# Once Expeditor finshes executing this script, it will commit the changes and push
+# the commit as a new tag corresponding to the value in the VERSION file.
