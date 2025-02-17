d6244175e6b2fa85bd0c24d531ecc76bd27b97ae
Imanol-Mikel Barba Sabariego <imanol@imanolbarba.net>
2025-02-17 15:09:13 +0100

    Better cgroupv2 check
    Not only check there's _a_ cgroupv2 tree mounted, but check it's also in the expected path
    
    Signed-off-by: Imanol-Mikel Barba Sabariego <imanol@imanolbarba.net>

--- lib/mixlib/shellout/unix.rb
+++ lib/mixlib/shellout/unix.rb
@@ -317,7 +317,7 @@ module Mixlib
       end
 
       def cgroupv2_available?
-        !File.read("/proc/mounts")[/^cgroup2/].nil?
+        !File.read("/proc/mounts")[%r{^cgroup2 /sys/fs/cgroup}].nil?
       end
 
       def fork_subprocess
